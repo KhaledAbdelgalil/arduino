d17b1f291f58cb5bfbfce6d4931934c90d61b2f4
khaledlooda <36304731+khaledlooda@users.noreply.github.com>
2018-02-09 20:26:13 +0100

    Add files via upload

---
+++ writing_on_serial_monitor_code.ino
@@ -0,0 +1,14 @@
+float value = 1.1;
+void setup() {
+  Serial.begin(9600); 
+}
+void loop() 
+{ 
+  value = value - 0.1; 
+  if( value == 0)
+  Serial.println("The value is exactly zero");
+  else if(fabs(value) < .0001) Serial.println("The value is close enough to zero");
+  else
+Serial.println(value); 
+delay(1000);
+}
